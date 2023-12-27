e926318b8cfbacc5b341cbafbca7438cbdaf73f9
sampson2448 <sampson2448@yahoo.com>
2023-12-27 05:59:41 -0500

    added background for quiz game

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 import {mainQuotes} from "../data/quotes.js"
 console.log(mainQuotes)
 /*-------------------------------- Constants --------------------------------*/
-
 /*---------------------------- Variables (state) ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
+const page = document.querySelector("#container")
+console.log(page.childNodes)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -12,4 +13,13 @@ console.log(mainQuotes)
 
 /*-------------------------------- Functions --------------------------------*/
 
+
+let rules = (e)=>{
+  
+  let p = document.createElement("p");
+  p.innerText= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit mollitia voluptates inventore veniam odio exercitationem quas. Id recusandae officia commodi? Facere in ipsam magni perferendis repellat eaque consectetur porro quo, cupiditate, fuga quasi atque sequi architecto odio laborum ab."
+  page.append(p);
+  
+  console.log(div.childNodes);
+}
 
