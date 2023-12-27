75d8b9b48c50ba10022ee0168205f57509ecff82
sampson2448 <sampson2448@yahoo.com>
2023-12-27 18:55:38 -0500

    added a function that display rules once clicked

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ console.log(mainQuotes)
 /*------------------------ Cached Element References ------------------------*/
 const btn = document.querySelector(".btn")
 const welcome= document.querySelector("#welcome")
-
+const body= document.querySelector("body")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -14,8 +14,14 @@ const welcome= document.querySelector("#welcome")
 /*-------------------------------- Functions --------------------------------*/
 btn.addEventListener("click",(e)=>{
   console.log(e.target)
-   welcome.className="hide"
+   welcome.classList.add("hide")
+   rules()
    
 })
 
-
+function rules(){
+  let div= document.createElement("div")
+  div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit. </p>"
+  div.classList.add("game-font")
+  welcome.parentElement.append(div)
+}
