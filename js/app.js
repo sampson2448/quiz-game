a4c8027ac40f4cba80452b7a791584a64c24b721
sampson2448 <sampson2448@yahoo.com>
2023-12-27 20:26:19 -0500

    added transition to rules

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
-import {mainQuotes} from "../data/quotes.js"
-console.log(mainQuotes)
 /*-------------------------------- Constants --------------------------------*/
-/*---------------------------- Variables (state) ----------------------------*/
+import {mainQuotes} from "../data/quotes.js"
+
+/*---------------------------- Variables ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
-const btn = document.querySelector(".btn")
+const btn = document.querySelector("#shifting-button")
 const welcome= document.querySelector("#welcome")
-const body= document.querySelector("body")
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
-/*-------------------------------- Functions --------------------------------*/
+console.log(btn.innerText)
 btn.addEventListener("click",(e)=>{
-  console.log(e.target)
-   welcome.classList.add("hide")
-   rules()
-   
+  if( btn.innerText == "Rules Of The Game" ){
+    hideContent()
+    renderRules() 
+  }else{
+    console.log("it's changed")
+  }
 })
 
-function rules(){
+/*-------------------------------- Functions --------------------------------*/
+
+
+function renderRules(){
   let div= document.createElement("div")
-  div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit. </p>"
   div.classList.add("game-font")
+  div.setAttribute('id','rendered-rules')
+  div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
   welcome.parentElement.append(div)
+}
+
+
+let hideContent = ()=>{
+  welcome.classList.add("hide")
+  btn.innerText= "start the game"
+}
+let showContent = ()=>{
+
 }
