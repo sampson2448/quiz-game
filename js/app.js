1225319f26517d995bd9b1461c9a81ab4e595c63
sampson2448 <sampson2448@yahoo.com>
2023-12-28 08:59:38 -0500

    added render game function

--- js/app.js
+++ js/app.js
@@ -6,15 +6,17 @@ import {mainQuotes} from "../data/quotes.js"
 /*------------------------ Cached Element References ------------------------*/
 const btn = document.querySelector("#shifting-button")
 const welcome= document.querySelector("#welcome")
-
 /*----------------------------- Event Listeners -----------------------------*/
 console.log(btn.innerText)
 btn.addEventListener("click",(e)=>{
   if( btn.innerText == "Rules Of The Game" ){
-    hideContent()
+    hideContent(welcome)
     renderRules() 
   }else{
-    console.log("it's changed")
+    const rules=document.querySelector('#rendered-rules')
+    hideContent(rules)
+    hideContent(btn)
+    renderGame()
   }
 })
 
@@ -28,11 +30,20 @@ function renderRules(){
   div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
   welcome.parentElement.append(div)
 }
+function renderGame(){
+  let div= document.createElement("div")
+  div.classList.add("game-font")
+  div.setAttribute('id','')
+  div.innerHTML= "<p> connected</p>"
+  welcome.parentElement.append(div)
+}
 
 
-let hideContent = ()=>{
-  welcome.classList.add("hide")
+let hideContent = (e)=>{
+  console.log(e)
+  e.classList.add("hide")
   btn.innerText= "start the game"
+ 
 }
 let showContent = ()=>{
 
