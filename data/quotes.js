0687ed27b840ed550668de9394a93dfeb5dc1cd1
sampson2448 <sampson2448@yahoo.com>
2024-01-03 16:02:22 -0500

    completed all functions

--- data/quotes.js
+++ data/quotes.js
@@ -1,11 +1,13 @@
-export let quotesArray = [
-  {quote:"one",name:'ace',img:'https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG-Download-Image.png'},
-  {quote:"two",name:'Teron',img:'https://media.licdn.com/dms/image/D4E03AQFNuPUNobBXsg/profile-displayphoto-shrink_800_800/0/1691530436198?e=2147483647&v=beta&t=o019qzRquKm0ccZ8t7dElpJRffmRjgLFpqymWyxPTKk'},
-  {quote:"three",name:'b',img:'https://media.licdn.com/dms/image/D4E03AQFNuPUNobBXsg/profile-displayphoto-shrink_800_800/0/1691530436198?e=2147483647&v=beta&t=o019qzRquKm0ccZ8t7dElpJRffmRjgLFpqymWyxPTKk'},
-  {quote:"four",name:'c',img:'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww'},
-  {quote:"five",name:'d',img:'https://pngimg.com/d/pokemon_PNG51.png'},
-  {quote:"six",name:'e',img:'https://pngimg.com/d/pokemon_PNG52.png'},
-  {quote:"seven",name:'f',img:'https://i.pinimg.com/originals/85/1e/cf/851ecf1b1c06cb071c37fb3c6de2ea4a.jpg'},
-  {quote:"eight",name:'g',img:'https://cdn.freebiesupply.com/logos/large/2x/pokemon-17-logo-png-transparent.png'}
+ 
+ let quotesArray = [
+  {quote:"one",name:'one',img:'https://www.pngall.com/wp-content/uploads/5/Pokemon-Pikachu-PNG-Download-Image.png'},
+  {quote:"two",name:'two',img:'https://media.licdn.com/dms/image/D4E03AQFNuPUNobBXsg/profile-displayphoto-shrink_800_800/0/1691530436198?e=2147483647&v=beta&t=o019qzRquKm0ccZ8t7dElpJRffmRjgLFpqymWyxPTKk'},
+  {quote:"three",name:'three',img:'https://media.licdn.com/dms/image/D4E03AQFNuPUNobBXsg/profile-displayphoto-shrink_800_800/0/1691530436198?e=2147483647&v=beta&t=o019qzRquKm0ccZ8t7dElpJRffmRjgLFpqymWyxPTKk'},
+  {quote:"four",name:'four',img:'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww'},
+  {quote:"five",name:'five',img:'https://pngimg.com/d/pokemon_PNG51.png'},
+  {quote:"six",name:'six',img:'https://pngimg.com/d/pokemon_PNG52.png'},
+  {quote:"seven",name:'seven',img:'https://i.pinimg.com/originals/85/1e/cf/851ecf1b1c06cb071c37fb3c6de2ea4a.jpg'},
+  {quote:"eight",name:'eight',img:'https://cdn.freebiesupply.com/logos/large/2x/pokemon-17-logo-png-transparent.png'}
 ]
 
+export default quotesArray
--- js/app.js
+++ js/app.js
@@ -1,104 +1,192 @@
-/*-------------------------------- Constants --------------------------------*/
-import {quotesArray} from "../data/quotes.js"
-
-/*---------------------------- Variables ----------------------------*/
-let counter=0
-let timesUsed=0
-let theSelectedQuestion
-
-/*------------------------ Cached Element References ------------------------*/
-const btn = document.querySelector("#shifting-button")
-const firstPage= document.querySelector("#first-page")
-const welcome= document.querySelector("#welcome")
-const gameStarter= document.querySelector("#game-starter")
-const questionDisplayStarter= document.querySelector("#question-display-container")
-
-const questionDisplayDiv = document.querySelector('#question-display')
-let counterContainer= document.querySelector('#counter-container')
-let counterSpan = document.querySelector('#counter-value')
-let cardContainer= document.querySelector('.card-container')
+import quotesArray from "../data/quotes.js"
 
 
-/*----------------------------- Event Listeners -----------------------------*/
-btn.addEventListener("click",()=>{
-  if( btn.innerText == "Rules Of The Game" ){
-    firstPage.childNodes[1].remove()
-    btn.innerText= "start the game"
-    renderRules() 
-  }else{
-    const rules=document.querySelector('#rendered-rules')
-    hideContent(rules)
-    hideContent(btn)
-    renderGame()
-  }
-})
+    let welcome= document.querySelector('#welcome')
+    const questionDisplayDiv = document.querySelector('#question-display')
+    let theSelectedQuestion
+    let counter=0
+    let timesUsed=0
+    let cardContainer= document.querySelector('.card-container')
+    let counterContainer= document.querySelector('#counter-container')
+    let counterSpan = document.querySelector('#counter-value')
+    let myKeyDiv =document.querySelector('#my-keys')
+    let fireRound= document.querySelector('#fire-round-container')
+    let finalRules= document.querySelector('#final-rules')
+    const btn = document.querySelector("#shifting-button")
+    const firstPage= document.querySelector("#first-page")
 
-/*-------------------------------- Functions --------------------------------*/
+
+
+    btn.addEventListener("click",()=>{
+        if( btn.innerText == "Rules Of The Game" ){
+          hideContent(firstPage)
+          btn.innerText= "start the game"
+          renderRules() 
+        }else{
+          const rules=document.querySelector('#rendered-rules')
+          hideContent(rules)
+          hideContent(btn)
+          renderGame()
+        }
+      })
 
 
 function renderRules(){
-  let div= document.createElement("div")
-  div.classList.add("game-font")
-  div.setAttribute('id','rendered-rules')
-  div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
-  console.log(welcome.parentElement)
-  firstPage.parentElement.append(div)
-}
-
-
-function renderGame(){
-  showContent(counterContainer)
-  showContent(questionDisplayStarter)
-  showContent(gameStarter)
-}
-  // create cards
-  function createCard(e){
     let div= document.createElement("div")
-    div.classList.add("card-container")
-    e.forEach(element => {
-        let innerDiv = document.createElement("div")
-        innerDiv.setAttribute('class','game-font card')
-        innerDiv.innerHTML= `<img src=${element.img} alt=Avatar/><h4>${element.name}</h4><button onclick='clickedName("${element.quote}")'>My answer</button>`
-        div.append(innerDiv)       
-    });
-    return welcome.append(div)
-}
-function shuffle(allArr){
-  let shuffleFormula= Math.floor(Math.random()* allArr.length)
-  const shuffleQuotes = allArr.sort(() => shuffleFormula - Math.random()*quotesArray.length);
-  const shuffledArrayList=[]
-  for (let index = 0; index < 3; index++) {
-      const element = shuffleQuotes[index];
-     shuffledArrayList.push(element)
-      
+    div.classList.add("game-font")
+    div.setAttribute('id','rendered-rules')
+    div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
+    welcome.parentElement.append(div)
   }
-let randomQuotesFormula=Math.floor(Math.random()* shuffledArrayList.length)
-let selectedQuestion =shuffledArrayList[randomQuotesFormula]
-theSelectedQuestion = selectedQuestion
-displayQuestion(selectedQuestion)
-createCard(shuffledArrayList)
-}
+  
+  function renderGame(){
+    hideContent(btn) 
+    showContent(questionDisplayDiv)
+    showContent(welcome)
+    showContent(counterContainer)
+  }
 
-// Take onClick value and the question
-function clickedName(e){
-  isItRight(e,theSelectedQuestion.quote)
-  }
+
+    function shuffle(allArr){
+        let shuffleFormula= Math.floor(Math.random()* allArr.length)
+        const shuffleQuotes = allArr.sort(() => shuffleFormula - Math.random()*quotesArray.length);
+        const shuffledArrayList=[]
+        for (let index = 0; index < 3; index++) {
+            const element = shuffleQuotes[index];
+           shuffledArrayList.push(element)
+            
+        }
+    let randomQuotesFormula=Math.floor(Math.random()* shuffledArrayList.length)
+    let selectedQuestion =shuffledArrayList[randomQuotesFormula]
+    theSelectedQuestion = selectedQuestion
+    displayQuestion(selectedQuestion)
+    createCard(shuffledArrayList)
+    }
+
+  
+     // create cards
+    function createCard(e){
+        let div= document.createElement("div")
+        div.classList.add("card-container")
+        e.forEach(element => {
+            let innerDiv = document.createElement("div")
+            innerDiv.setAttribute('class','game-font card')
+            innerDiv.innerHTML= `<img src=${element.img} alt=Avatar/><h4>${element.name}</h4><button onclick='clickedName("${element.quote}")'>My answer</button>`
+            div.append(innerDiv)       
+        });
+        return welcome.append(div)
+    }
     // display the name of the question
     function displayQuestion(question){
-      questionDisplayDiv.innerHTML= `<h2>${question.quote}</h2>`
-      console.log(question)
- 
-     }
+     questionDisplayDiv.innerHTML= `<h2>${question.quote}</h2>`
+     console.log(question)
+
+    }
+// Take onClick value and the question
+document.clickedName =clickedName
+document.finalTest=finalTest
+function clickedName(e){
+isItRight(e,theSelectedQuestion.quote)
+}
+//remove cards
+function resetCard(){
+  const element= document.querySelector('.card-container')
+  element.remove()
+}
+
+ function isItRight(question,displayedQuestion){
+    if(displayedQuestion == question){
+     counter++
+    counterDisplay(counter)
+    resetCard()
+    shuffle(quotesArray)
+
+    } if(displayedQuestion == question && counter==3){
+        resetCard()
+        showContent(questionDisplayDiv)
+        finalTestRules()
+    }
+    if(displayedQuestion != question){
+        counter =0
+        counterDisplay(counter)
+        resetCard()
+        shuffle(quotesArray)
+    }
+
+ }
+ function counterDisplay(counter){
+    counterSpan.innerText=counter
+ }
+
+
+ function finalTest(e){
+showContent(fireRound.parentElement)
+hideContent(welcome)
+hideContent(counterContainer)
+
+const body=document.querySelector('body')
+let button= document.createElement("button")
+button.classList.add('restart-button')
+button.classList.add('game-font')
+
+button.onclick=function(){
+    location.reload()
+}
+
+if(e=='Reindeer'){
+winOrLose()
+button.innerText='You win, try again?'
+body.style.backgroundImage="url('./imgs/win.gif')"
+body.style.backgroundRepeat = "no-repeat" 
+body.style.backgroundPosition = "bottom";  // The background image will be centered
+body.style.backgroundSize = "cover"
+body.style.height="100vh"
+body.append(button)
+
+
+
+}if(e=='German Shepherd'){
+ winOrLose()
+body.style.backgroundImage="url('./imgs/lose.gif')"
+button.innerText='Start Over'
+body.style.backgroundRepeat = "no-repeat" 
+body.style.backgroundPosition = "center";  // The background image will be centered
+body.style.backgroundSize = "cover"
+body.style.height="100vh"
+body.append(button)
+}
+
+
+}
+
+ function finalTestRules(){
+    hideContent(questionDisplayDiv)
+    counterContainer.innerText= "You have cleared the 3 in a row condidtion"
+    let testDiv= document.createElement("div")
+    testDiv.classList.add("card-container")
+    testDiv.innerHTML= `<div id='final-rules'><h1> Final Round: click the correct keys to win.<br/> You lose if not entered correctly and or time runs out<br/> <button id='timer' onclick='finalTest()'>I understand</button></h1></div>`
+    welcome.append(testDiv)
+}
+
+
 
 let hideContent = (e)=>{
-  console.log(e)
-  e.classList.add("hide")
- 
-}
+    e.classList.add("hide")
+  }
+  let showContent = (e)=>{
+  e.classList.remove('hide')
+  }
 
-let showContent = (e)=>{
-  e.classList.remove("hide")
-
-}
-
+ console.log(counter)
 shuffle(quotesArray)
+
+
+function winOrLose(){
+    hideContent(fireRound.parentElement)
+
+}
+
+
+
+
+console.log(fireRound.parentElement)
