c368eb56a5f76be9a0dfad9e0b1c8f393e62e42c
sampson2448 <sampson2448@yahoo.com>
2024-01-03 07:37:08 -0500

    added shuffle array feature

--- js/app.js
+++ js/app.js
@@ -35,6 +35,8 @@ function renderRules(){
   div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
   welcome.parentElement.append(div)
 }
+
+
 function renderGame(){
   let div= document.createElement("div")
   div.classList.add("game-font")
@@ -44,6 +46,21 @@ function renderGame(){
   showContent(counterContainer)
 }
 
+function shuffle(allArr){
+  let shuffleFormula= Math.floor(Math.random()* allArr.length)
+  const shuffleQuotes = allArr.sort(() => shuffleFormula - Math.random()*quotesArray.length);
+  const shuffledArrayList=[]
+  for (let index = 0; index < 3; index++) {
+      const element = shuffleQuotes[index];
+     shuffledArrayList.push(element)
+      
+  }
+let randomQuotesFormula=Math.floor(Math.random()* shuffledArrayList.length)
+let selectedQuestion =shuffledArrayList[randomQuotesFormula]
+theSelectedQuestion = selectedQuestion
+displayQuestion(selectedQuestion)
+createCard(shuffledArrayList)
+}
 
 let hideContent = (e)=>{
   console.log(e)
