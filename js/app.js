717cfc813aba73113f681d2eead7f7f1739fd9d0
sampson2448 <sampson2448@yahoo.com>
2024-01-04 06:20:25 -0500

    Updated the rules section

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function renderRules(){
     let div= document.createElement("div")
     div.classList.add("game-font")
     div.setAttribute('id','rendered-rules')
-    div.innerHTML= "<p> 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi debitis molestiae voluptatum voluptatibus deleniti eos, asperiores culpa fugit dolorem fuga doloribus accusantium ea animi quaerat!<br><br> 2. Fuga aspernatur asperiores repudiandae reprehenderit est. Velit ut nesciunt voluptatum aperiam, tempore placeat vel blanditiis, minus sunt aut nemo facere dolore id praesentium similique nam.<br><br> 3.Debitis earum adipisci eius ex tenetur velit nobis, obcaecati, ea porro esse voluptatem placeat facere harum sit cupiditate nemo qui asperiores doloribus aliquid? Non, sit.</p>"
+    div.innerHTML= `<p>1.Click the my answer button to select the person you felt create the quote<br/>2.In order to win the game, you must have three answers correct in a row. <br/>3. Good Luck and have fun!</p>`
     welcome.parentElement.append(div)
   }
   
